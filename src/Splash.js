526efdbf313cf07332a4cbfd7e1411a23ece12b7
Swaraj Dhumne <swaraj.dhumne@gmail.com>
2017-10-28 19:49:12 -0400

    added flashy stuff

--- src/Splash.js
+++ src/Splash.js
@@ -5,13 +5,15 @@ import './splash.css';
 class Splash extends Component {
   render() {
     return (
-      <div className="Splash--body">
-        <h1> Finance_Me </h1>
-        <h3> Have you ever wondered what your parents do with all the money? </h3>
-        <h4> You can play this game and learn why they are saying NO to all your requests. </h4>
-        <div className="Splash--button-area">
-          <Button bsStyle="primary">Play Now</Button>
-          <Button bsStyle="link">Take the tutorial</Button>
+      <div className="Splash--background">
+        <div className="Splash--body">
+          <h1> Rock 101 </h1>
+          <h3> Have you ever wondered what your parents do with all the money? </h3>
+          <h3> You can play this game and learn why they are saying NO to all your requests. </h3>
+          <div className="Splash--button-area">
+            <Button bsStyle="primary" bsSize="large">Play Now</Button>
+            <Button bsStyle="link" bsSize="large" className="Splash--button-area-tutorial">Take the tutorial</Button>
+          </div>
         </div>
       </div>
     );
