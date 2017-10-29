ce9b69cf37b90366b15ed692a4929ba427b31699
Dhruv Patel <dhruvp5@vt.edu>
2017-10-29 11:37:24 -0400

    added normal evts

--- src/Events.js
+++ src/Events.js
@@ -34,23 +34,23 @@ function bonus() {
   switch (x) {
     case 1:
       bonusRock = Math.floor((Math.random() * 100) + 50);
-      outputText = bonusRock + " bonus? This rocks! (Gained " + bonusRock + " rocks)";
+      outputText = bonusRock + " bonus? This rocks! (Gained " + bonusRock + " rocks.)";
       break;
     case 2:
       bonusRock = Math.floor((Math.random() * 100) + 50);
       outputText = "A friend gave you " + bonusRock + " rocks. Do not spend all in "
-        + "one place. Gained " + bonusRock + " rocks";
+        + "one place. (Gained " + bonusRock + " rocks.)";
       break;
     case 3:
       bonusJoy = Math.floor((Math.random() * 10) + 5);
       outputText = "You got a compliment! That rocked your world. " +
-        "Gained " + bonusJoy + " joy";
+        "(Gained " + bonusJoy + " joy.)";
       break;
     case 4:
       bonusRock = Math.floor((Math.random() * 100) + 50);
       bonusJoy = Math.floor((Math.random() * 10) + 5);
       outputText = "You won rock lottery. Solid stuff! " +
-        "Gained " + bonusRock + " rocks AND gained " + bonusJoy + " joy";
+        "(Gained " + bonusRock + " rocks AND gained " + bonusJoy + " joy.)";
       break;
     default:
       outputText = "You rocked out of control!";
@@ -79,22 +79,23 @@ function negatives() {
       bonusRock = Math.floor((Math.random() * 100) + 50);
       bonusJoy = Math.floor((Math.random() * 10) + 5);
       outputText = "You took your health for granite. You need to go to the emergency room. " +
-        "Lose "+ bonusRock + " rocks AND " + bonusJoy + " joy";
+        "(Lose "+ bonusRock + " rocks AND " + bonusJoy + " joy.)";
       break;
     case 2:
       bonusRock = Math.floor((Math.random() * 100) + 50);
       outputText = "You igneous the consequences, your suit pants split and now you need new pants. " +
-        "Lose "+ bonusRock + " rocks";
+        "(Lose "+ bonusRock + " rocks.)";
       break;
     case 3:
       bonusRock = Math.floor((Math.random() * 100) + 50);
+      bonusJoy = Math.floor((Math.random() * 10) + 5);
       outputText = "Oh my geode, the concert was cancelled. No rock refunds. " +
-        "You lose " + bonusRock + " rocks";
+        "(Lose "+ bonusRock + " rocks AND " + bonusJoy + " joy.)";
       break;
     case 4:
       bonusRock = Math.floor((Math.random() * 100) + 50);
       outputText = "It seems cave security was not very concrete. " +
-        "You lose " + bonusRock + " rocks";
+        "(Lose " + bonusRock + " rocks.)";
       break;
     default:
       outputText = "You rocked out of control!";
@@ -103,8 +104,8 @@ function negatives() {
   }
 
   output.push(outputText);
-  output.push(bonusRock);
-  output.push(bonusJoy);
+  output.push(-1 * bonusRock);
+  output.push(-1 * bonusJoy);
   output.push(decisionText);
   output.push(promotionRock);
   return output;
@@ -123,19 +124,28 @@ function tradeoffs() {
       bonusRock = Math.floor((Math.random() * 100) + 100);
       bonusJoy = Math.floor((Math.random() * 10) + 10);
       outputText = "You can rock overtime. Sadder time but more rock. Is it worth? " +
-        "Get " + bonusRock + " rocks AND lose " + bonusJoy + " joy";
+        "(Get " + bonusRock + " rocks AND lose " + bonusJoy + " joy.)";
+      output.push(outputText);
+      output.push(bonusRock);
+      output.push(-1 * bonusJoy);
       break;
     case 2:
       bonusRock = Math.floor((Math.random() * 100) + 100);
       bonusJoy = Math.floor((Math.random() * 10) + 10);
       outputText = "Time for vacation. You need time off. Go to rock and roll concert? " +
-        "Get " + bonusJoy + " joy AND lose " + bonusRock + " rocks";
+        "(Get " + bonusJoy + " joy AND lose " + bonusRock + " rocks.)";
+      output.push(outputText);
+      output.push(-1 * bonusRock);
+      output.push(bonusJoy);
       break;
     case 3:
       bonusRock = Math.floor((Math.random() * 100) + 100);
       bonusJoy = Math.floor((Math.random() * 10) + 10);
       outputText = "Go on a gneiss rock date. " +
-        "Get " + bonusJoy + " joy AND lose " + bonusRock + " rocks";
+        "(Get " + bonusJoy + " joy AND lose " + bonusRock + " rocks.)";
+      output.push(outputText);
+      output.push(-1 * bonusRock);
+      output.push(bonusJoy);
       break;
     default:
       outputText = "You rocked out of control!";
@@ -146,9 +156,6 @@ function tradeoffs() {
   decisionText.push("Yes");
   decisionText.push("No");
 
-  output.push(outputText);
-  output.push(bonusRock);
-  output.push(bonusJoy);
   output.push(decisionText);
   output.push(promotionRock);
   return output;
@@ -166,22 +173,31 @@ function decisions() {
       bonusRock = Math.floor((Math.random() * 100) + 100);
       bonusJoy = Math.floor((Math.random() * 10) + 10);
       outputText = "Buy a new rocking chair or invest in the rock market?";
-      decisionText.push("Gain " + bonusJoy + " joy");
-      decisionText.push("Gain " + bonusRock + " rocks");
+      decisionText.push("Gain " + bonusJoy + " joy.");
+      decisionText.push("Gain " + bonusRock + " rocks.");
+      output.push(outputText);
+      output.push(bonusRock);
+      output.push(bonusJoy);
       break;
     case 2:
       bonusRock = Math.floor((Math.random() * 100) + 100);
       bonusJoy = Math.floor((Math.random() * 10) + 10);
       outputText = "You lost rock coat, tough luck. Buy a new coat or catch a coal?";
-      decisionText.push("Lose " + bonusJoy + " joy");
-      decisionText.push("Lose " + bonusRock + " rocks");
+      decisionText.push("Lose " + bonusJoy + " joy.");
+      decisionText.push("Lose " + bonusRock + " rocks.");
+      output.push(outputText);
+      output.push(-1 * bonusRock);
+      output.push(-1 * bonusJoy);
       break;
     case 3:
       bonusRock = Math.floor((Math.random() * 100) + 100);
       bonusJoy = Math.floor((Math.random() * 10) + 10);
       outputText = "Jackpot! Found pile of rocks! Find and return to the owner or keep the rocks?";
-      decisionText.push("Gain " + bonusJoy + " joy");
-      decisionText.push("Gain " + bonusRock + " rocks");
+      decisionText.push("Gain " + bonusJoy + " joy.");
+      decisionText.push("Gain " + bonusRock + " rocks.");
+      output.push(outputText);
+      output.push(bonusRock);
+      output.push(bonusJoy);
       break;
     default:
       outputText = "You rocked out of control!";
@@ -189,9 +205,6 @@ function decisions() {
       bonusJoy = -1;
   }
 
-  output.push(outputText);
-  output.push(bonusRock);
-  output.push(bonusJoy);
   output.push(decisionText);
   output.push(promotionRock);
   return output;
@@ -210,6 +223,11 @@ function performance() {
       outputText = "\"You work hard. Rock is hard. That makes you as good as rock.\" - Boss. " +
         "Get Promotion! " +
         "Now recieve " + promotionRock + " more rocks every two months.";
+      output.push(outputText);
+      output.push(bonusRock);
+      output.push(bonusJoy);
+      output.push(decisionText);
+      output.push(promotionRock);
       break;
 
     case 2:
@@ -217,6 +235,11 @@ function performance() {
       outputText = "\"You lack skill. You deserve less rocks.\" - Boss. " +
         "Get Demotion! " +
         "Now recieve " + promotionRock + " less rocks every two months.";
+      output.push(outputText);
+      output.push(bonusRock);
+      output.push(bonusJoy);
+      output.push(decisionText);
+      output.push(-1 * promotionRock);
       break;
     default:
       outputText = "You rocked out of control!";
@@ -224,11 +247,6 @@ function performance() {
       bonusJoy = -1;
   }
 
-  output.push(outputText);
-  output.push(bonusRock);
-  output.push(bonusJoy);
-  output.push(decisionText);
-  output.push(promotionRock);
   return output;
 }
 
